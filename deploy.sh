09a4a60237b741750623e001bc29d54ee0806d60
Christophe De Troyer <christophe.detroyer@gmail.com>
2018-09-02 23:33:22 +0200

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,12 @@
 MIX_ENV=prod
 
 mix deps.get --only prod
-mix compile
-mix ecto.migrate 
 
 cd assets 
 node_modules/brunch/bin/brunch build --production
 
 cd ..
 mix phx.digest 
+
+mix ecto.migrate
+mix phx.digest
