3ee80544146673eb5de3e33d8553ecb167f5ddc1
Christophe De Troyer <christophe.detroyer@gmail.com>
2018-09-03 10:26:37 +0200

    deployment

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :exbin, ExBinWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4001],
   debug_errors: false,
   code_reloader: true,
   check_origin: false,
