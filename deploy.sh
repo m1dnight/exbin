224fa2f402b1efe63b243ccfbb4b4ecee4d9d983
Christophe De Troyer <christophe.detroyer@gmail.com>
2018-09-03 09:58:51 +0200

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,28 @@
 
 MIX_ENV=prod
 
+git pull origin master 
+
 mix deps.get --only prod
 
 cd assets 
+
 npm install --production
+
 node_modules/brunch/bin/brunch build --production
+
 cd ..
+
+mix ecto.migrate 
 
 mix phx.digest 
 
-mix ecto.migrate
-mix phx.digest
+
+
+# git pull origin master 
+# mix deps.get
+# cd assets 
+# npm install 
+# node_modules/brunch/bin/brunch build 
+# cd ..
+# iex -S mix phoenix.server
