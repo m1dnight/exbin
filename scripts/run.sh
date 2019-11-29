70216bcbf9e8b98ca668e4f5bf6926f0d3274b8c
Christophe De Troyer <christophe.detroyer@gmail.com>
2019-11-29 15:38:13 +0100

    Update run script for dev stuff

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,9 @@
 docker network rm exbinnet 
 docker network create exbinnet 
 
+docker rm -f exbindb 
+docker rm -f pg-tmp
+
 # Run the database 
 ################################################################################
 
@@ -35,34 +38,34 @@ docker run --rm                        \
            postgres                    \
            psql -h exbindb -U postgres -c "CREATE DATABASE exbindb OWNER exbin;"
 
-# Run ExBin 
-################################################################################
-docker rm -f exbin 
+# # Run ExBin 
+# ################################################################################
+# docker rm -f exbin 
 
-docker run -it --rm                      \
-           --name exbin                  \
-           --net=exbinnet                \
-           -e DB_USER='exbin'            \
-           -e DB_PASS='exbin'            \
-           -e DB_HOST='exbindb.exbinnet' \
-           -e DB_NAME='exbindb'          \
-           -e EXTERNAL_URL='localhost'   \
-           -e TCP_PORT='9999'            \
-           -e TCP_IP='127.0.0.1'         \
-           -e PORT=4000                  \
-           -p 4000:4000                  \
-           m1dnight/exbin ecto.migrate
+# docker run -it --rm                      \
+#            --name exbin                  \
+#            --net=exbinnet                \
+#            -e DB_USER='exbin'            \
+#            -e DB_PASS='exbin'            \
+#            -e DB_HOST='exbindb.exbinnet' \
+#            -e DB_NAME='exbindb'          \
+#            -e EXTERNAL_URL='localhost'   \
+#            -e TCP_PORT='9999'            \
+#            -e TCP_IP='127.0.0.1'         \
+#            -e PORT=4000                  \
+#            -p 4000:4000                  \
+#            m1dnight/exbin ecto.migrate
            
-docker run -d                            \
-           --name exbin                  \
-           --net=exbinnet                \
-           -e DB_USER='exbin'            \
-           -e DB_PASS='exbin'            \
-           -e DB_HOST='exbindb.exbinnet' \
-           -e DB_NAME='exbindb'          \
-           -e EXTERNAL_URL='localhost'   \ 
-           -e TCP_PORT='9999'            \
-           -e TCP_IP='127.0.0.1'         \
-           -e PORT=4000                  \
-           -p 4000:4000                  \
-           m1dnight/exbin phx.server         
+# docker run -d                            \
+#            --name exbin                  \
+#            --net=exbinnet                \
+#            -e DB_USER='exbin'            \
+#            -e DB_PASS='exbin'            \
+#            -e DB_HOST='exbindb.exbinnet' \
+#            -e DB_NAME='exbindb'          \
+#            -e EXTERNAL_URL='localhost'   \ 
+#            -e TCP_PORT='9999'            \
+#            -e TCP_IP='127.0.0.1'         \
+#            -e PORT=4000                  \
+#            -p 4000:4000                  \
+#            m1dnight/exbin phx.server         
