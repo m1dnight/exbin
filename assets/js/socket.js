6ad9777ac2f19af0be4b96e47f49eb85285ce1f8
Christophe De Troyer <christophe.detroyer@gmail.com>
2020-09-24 16:27:28 +0200

    cleanup

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -4,92 +4,73 @@ import {
 
 let socket = new Socket("/socket", {
   params: {
-    token: window.userToken
+      token: window.userToken
   }
 })
 
-
-
-document.addEventListener("DOMContentLoaded", function (event) {
-
+document.addEventListener("DOMContentLoaded", function(event) {
   var element = document.getElementById("myEditor");
   //If it isn't "undefined" and it isn't "null", then it exists.
-  if (typeof (element) != 'undefined' && element != null) {
-    ////////////////////////////////////////////////////////////////////////////
-    // Setup socket
-    var padId = document.getElementById("padId").getAttribute("value");
+  if (typeof(element) != 'undefined' && element != null) {
+      ////////////////////////////////////////////////////////////////////////////
+      // Setup socket
+      var padId = document.getElementById("padId").getAttribute("value");
+      let channel = socket.channel(`sync:${padId}`, {})
 
+      ////////////////////////////////////////////////////////////////////////////
+      // Setup editor.
+      var socket_change = false;
+      YUI().use(
+          'aui-ace-editor',
+          function(Y) {
+              var editor = new Y.AceEditor({
+                  boundingBox: '#myEditor',
+                  mode: 'scheme',
+              }).render();
 
-    let channel = socket.channel(`sync:${padId}`, {})
+              var editor = ace.edit("myEditor");
+              editor.setTheme("ace/theme/chaos");
+              editor.setShowPrintMargin(false);
 
+              editor.session.on('change', function(delta) {
+                  console.log("Change callback triggered! From socket: ", socket_change)
+                  if (socket_change) {
 
+                  } else {
+                      channel.push("change", delta);
+                      channel.push("state", editor.getValue())
+                  }
+              })
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Setup editor.
-    var socket_change = false;
-    YUI().use(
-      'aui-ace-editor',
-      function (Y) {
-        var editor = new Y.AceEditor({
-          boundingBox: '#myEditor',
-          mode: 'scheme',
-        }).render();
+              //////////////////////////////////////////////////////////////////
+              // Connect to server.
+              socket.connect()
+              channel.join()
+                  .receive("error", resp => {
+                      console.log("Unable to connect to channel.", resp)
+                  })
 
-        var editor = ace.edit("myEditor");
-        editor.setTheme("ace/theme/chaos");
-        editor.setShowPrintMargin(false);
+              //////////////////////////////////////////////////////////////////
+              // Event handlers
 
-        editor.session.on('change', function (delta) {
-          console.log("Change callback triggered! From socket: ", socket_change)
-          if (socket_change) {
+              // Local change.
+              channel.on("change", (resp) => {
+                  socket_change = true;
+                  var editor = ace.edit("myEditor");
+                  let arr = new Array(resp.data);
+                  editor.getSession().getDocument().applyDeltas(arr);
+                  socket_change = false;
+              })
 
-          }
-          else {
-            channel.push("change", delta);
-            channel.push("state", editor.getValue())
-          }
-        })
-
-        // Join the channel here, otherwise we might join and get state before the editor is loaded.
-        socket.connect()
-
-        channel.join()
-          .receive("error", resp => {
-            console.log("Unable to connect to channel.", resp)
-          })
-
-        channel.on("add_some_text", (resp) => {
-          socket_change = true;
-          var editor = ace.edit("myEditor");
-          var value = editor.getValue() + ".";
-          editor.setValue(value)
-          editor.clearSelection();
-          socket_change = false;
-        })
-
-        channel.on("change", (resp) => {
-          socket_change = true;
-          var editor = ace.edit("myEditor");
-          let arr = new Array(resp.data);
-          editor.getSession().getDocument().applyDeltas(arr);
-          socket_change = false;
-        })
-
-        channel.on("init", (resp) => {
-          console.log("Incoming init", resp);
-
-          socket_change = true;
-          var editor = ace.edit("myEditor");
-          editor.setValue(resp.state);
-        })
-
-
-      }
-    );
-
-
-
-
+              // Initial text content.
+              channel.on("init", (resp) => {
+                  socket_change = true;
+                  var editor = ace.edit("myEditor");
+                  editor.setValue(resp.state);
+                  editor.clearSelection();
+                  socket_change = false;
+              });
+          });
   }
 });
 
