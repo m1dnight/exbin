35d59c4ca4e5d61aec203c3eda4d2f080ceca8e1
Christophe De Troyer <christophe.detroyer@gmail.com>
2021-07-22 13:17:01 +0200

    request

--- lib/exbin_web/controllers/fallback_controller.ex
+++ lib/exbin_web/controllers/fallback_controller.ex
@@ -8,20 +8,27 @@ defmodule ExBinWeb.FallbackController do
 
   # This clause handles errors returned by Ecto's insert/update/delete.
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
-
     conn
     |> put_status(:unprocessable_entity)
     |> put_view(ExBinWeb.ChangesetView)
     |> render("error.json", changeset: changeset)
   end
 
-  # This clause is an example of how to handle resources that cannot be found.
+  # This clause is for when an invalid snippet is requested.
   def call(conn, {:error, :not_found}) do
-    IO.inspect("call, #{inspect {:error, :not_found}}", label: "call")
+    IO.inspect("call, #{inspect({:error, :not_found})}", label: "call")
 
     conn
     |> put_status(:not_found)
     |> put_view(ExBinWeb.ErrorView)
     |> render(:"404")
+  end
+
+  # This clause is triggered when an invalid snippet is sent.
+  def call(conn, {:error, _e}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(ExBinWeb.ErrorView)
+    |> render(:"400")
   end
 end
