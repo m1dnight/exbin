81f07697218575877d5e2b50b0906ae6a2512f61
Josh Proehl <josh@daedalusdreams.com>
2021-08-17 10:02:02 -0500

    Add SnippetTest and snippet factory

---
+++ test/exbin/snippet_test.exs
@@ -0,0 +1,39 @@
+defmodule ExBin.SnippetTest do
+  use ExBin.DataCase, async: true
+  alias ExBin.{Repo, Snippet}
+  import ExBin.Factory
+
+  test "errors if name is empty" do
+    changeset = Snippet.changeset(build(:snippet, %{name: ""}))
+    assert %{name: ["can't be blank"]} = errors_on(changeset)
+  end
+
+  test "errors if name is not unique" do
+    insert!(:snippet, %{name: "DupeSnippet"})
+    assert %Snippet{name: "DupeSnippet"} = Repo.one(Snippet)
+    dupe_changeset = Snippet.changeset(%Snippet{}, %{name: "DupeSnippet"})
+    {:error, error_cs} = Repo.insert(dupe_changeset)
+    assert %{name: ["has already been taken"]} = errors_on(error_cs)
+  end
+
+  test "defaults content to an empty string" do
+    insert!(:snippet, %{name: "BasicSnippet"})
+    assert %Snippet{name: "BasicSnippet", content: ""} = Repo.one(Snippet)
+  end
+
+  test "defaults viewcount to 0" do
+    insert!(:snippet, %{name: "NeverViewed"})
+    assert %Snippet{viewcount: 0, name: "NeverViewed"} = Repo.one(Snippet)
+  end
+
+  test "defaults private to true" do
+    insert!(:snippet, %{name: "ItsPrivate"})
+    assert %Snippet{private: true, name: "ItsPrivate"} = Repo.one(Snippet)
+  end
+
+  test "defaults ephemeral to false" do
+    insert!(:snippet, %{name: "Ghostly"})
+    assert %Snippet{ephemeral: false, name: "Ghostly"} = Repo.one(Snippet)
+  end
+
+end
