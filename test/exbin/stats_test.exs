81e12b4ca81bd2c813dcf2b156699573891c8b15
Josh Proehl <josh@daedalusdreams.com>
2021-08-18 13:37:28 -0500

    Add tests for count_per_month expected behaviours

--- test/exbin/stats_test.exs
+++ test/exbin/stats_test.exs
@@ -3,6 +3,15 @@ defmodule ExBin.StatsTest do
   alias ExBin.{Repo, Snippet, Stats, Clock}
   import ExBin.Factory
 
+  @test_zone "Europe/Brussels" # Tests are running in UTC+2 timezone
+  @utc_zone "Etc/UTC"
+
+  test "ensure we're in the right timezone for the tests to work" do
+    # We're going to hard-code some values to check against, so these tests are
+    # only expected to work in that zone. So we'll do a quick sanity check
+    # before we run the rest of the tests.
+    assert Application.get_env(:exbin, :timezone) == @test_zone
+  end
 
   describe "average_length/0" do
     test "returns 0.0 if no snippets in DB" do
@@ -94,23 +103,159 @@ defmodule ExBin.StatsTest do
 
   describe "count_per_month/0" do
     test "returns desired result structure if no snippets in DB" do
+      # We're gong to pick a time that is unambiguous in any time zone as it relates to the "beginning of the month".
+      # This allows us to hard-code a resulting structure to compare to.
+      Clock.freeze(~U[2021-08-15 12:00:00.000000Z])
+      empty_count = Stats.count_per_month
 
+      assert empty_count == [
+          {~N[2020-09-01 00:00:00], {0,0}},
+          {~N[2020-10-01 00:00:00], {0,0}},
+          {~N[2020-11-01 00:00:00], {0,0}},
+          {~N[2020-12-01 00:00:00], {0,0}},
+          {~N[2021-01-01 00:00:00], {0,0}},
+          {~N[2021-02-01 00:00:00], {0,0}},
+          {~N[2021-03-01 00:00:00], {0,0}},
+          {~N[2021-04-01 00:00:00], {0,0}},
+          {~N[2021-05-01 00:00:00], {0,0}},
+          {~N[2021-06-01 00:00:00], {0,0}},
+          {~N[2021-07-01 00:00:00], {0,0}},
+          {~N[2021-08-01 00:00:00], {0,0}}
+        ]
     end
 
-    test "returns correct results for user time zone if snippet is created just before month rollover in UTC time" do
+    test "does not return results for snippets created before the begining of the 11th ago month" do
+      # If it is 2021-08-15, we we should be getting results from 2020-09-01 and forward.
+      # Results before 2020-09-01 should not be included in any bucket, even if they are within a year of "now"
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2018-06-16 12:00:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2018-07-16 12:00:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2019-08-31 12:00:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2019-09-01 23:00:00.000000], @utc_zone)})
+      # This one should be in our result, because it's in 2020-09 in "our" time zone.
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2020-08-31 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2020-09-05 12:00:00.000000], @utc_zone)})
 
+      Clock.freeze(~U[2021-08-15 12:00:00.000000Z])
+      assert Stats.count_per_month == [
+          {~N[2020-09-01 00:00:00], {1,1}},
+          {~N[2020-10-01 00:00:00], {0,0}},
+          {~N[2020-11-01 00:00:00], {0,0}},
+          {~N[2020-12-01 00:00:00], {0,0}},
+          {~N[2021-01-01 00:00:00], {0,0}},
+          {~N[2021-02-01 00:00:00], {0,0}},
+          {~N[2021-03-01 00:00:00], {0,0}},
+          {~N[2021-04-01 00:00:00], {0,0}},
+          {~N[2021-05-01 00:00:00], {0,0}},
+          {~N[2021-06-01 00:00:00], {0,0}},
+          {~N[2021-07-01 00:00:00], {0,0}},
+          {~N[2021-08-01 00:00:00], {0,0}}
+        ]
+    end
+
+    # Note, this was the test that exposed the errors. Every other test worked, but this one was just missing results.
+    # That is why this one has a bunch of extra data being checked. If they helped diagnose the problem the first time,
+    # they'll help guard against it in the future.
+    test "returns correct results for user time zone if snippet is created just before month rollover in UTC time" do
+      # This snippet should be in 2020-09 since it's in the middle of the month
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2020-09-15 23:55:00.000000], @utc_zone)})
+      # These snippets should be in the month bucket after their naive insert date, since it's 2 hours later in "our" timezone
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2020-08-31 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2020-08-31 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2020-09-30 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2020-10-31 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2020-11-30 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2020-12-31 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2021-01-31 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2021-02-28 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2021-03-31 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2021-03-31 23:56:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2021-03-31 23:55:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2021-03-31 23:56:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2021-06-30 23:55:00.000000], @utc_zone)})
+
+      Clock.freeze(~U[2021-08-15 12:00:00.000000Z])
+      assert Stats.count_per_month == [
+          {~N[2020-09-01 00:00:00], {1,2}},
+          {~N[2020-10-01 00:00:00], {1,0}},
+          {~N[2020-11-01 00:00:00], {1,0}},
+          {~N[2020-12-01 00:00:00], {1,0}},
+          {~N[2021-01-01 00:00:00], {1,0}},
+          {~N[2021-02-01 00:00:00], {1,0}},
+          {~N[2021-03-01 00:00:00], {1,0}},
+          {~N[2021-04-01 00:00:00], {2,2}},
+          {~N[2021-05-01 00:00:00], {0,0}},
+          {~N[2021-06-01 00:00:00], {0,0}},
+          {~N[2021-07-01 00:00:00], {1,0}},
+          {~N[2021-08-01 00:00:00], {0,0}}
+        ]
     end
 
     test "returns correct results for user time zone if snippet is created just after month rollover in UTC time" do
-
+      # This snippet should be in the month bucket of their naive insert date, since it's 2 hours later in "our" timezone
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2021-07-01 00:01:00.000000], @utc_zone)})
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2021-08-01 00:01:00.000000], @utc_zone)})
+      
+      Clock.freeze(~U[2021-08-15 12:00:00.000000Z])
+      assert Stats.count_per_month == [
+          {~N[2020-09-01 00:00:00], {0,0}},
+          {~N[2020-10-01 00:00:00], {0,0}},
+          {~N[2020-11-01 00:00:00], {0,0}},
+          {~N[2020-12-01 00:00:00], {0,0}},
+          {~N[2021-01-01 00:00:00], {0,0}},
+          {~N[2021-02-01 00:00:00], {0,0}},
+          {~N[2021-03-01 00:00:00], {0,0}},
+          {~N[2021-04-01 00:00:00], {0,0}},
+          {~N[2021-05-01 00:00:00], {0,0}},
+          {~N[2021-06-01 00:00:00], {0,0}},
+          {~N[2021-07-01 00:00:00], {1,0}},
+          {~N[2021-08-01 00:00:00], {0,1}}
+        ]
     end
 
     test "returns correct results for user time zone if snippet created just before month rollover in local time" do
+      # This snippet should be in bucket for their insert time, since they were inserted in "our" timezone
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2020-09-01 00:01:00.000000], @test_zone) |> DateTime.shift_zone!(@utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2021-07-01 00:01:00.000000], @test_zone) |> DateTime.shift_zone!(@utc_zone)})
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2021-08-01 00:01:00.000000], @test_zone) |> DateTime.shift_zone!(@utc_zone)})
 
+      Clock.freeze(~U[2021-08-15 12:00:00.000000Z])
+      assert Stats.count_per_month == [
+          {~N[2020-09-01 00:00:00], {1,0}},
+          {~N[2020-10-01 00:00:00], {0,0}},
+          {~N[2020-11-01 00:00:00], {0,0}},
+          {~N[2020-12-01 00:00:00], {0,0}},
+          {~N[2021-01-01 00:00:00], {0,0}},
+          {~N[2021-02-01 00:00:00], {0,0}},
+          {~N[2021-03-01 00:00:00], {0,0}},
+          {~N[2021-04-01 00:00:00], {0,0}},
+          {~N[2021-05-01 00:00:00], {0,0}},
+          {~N[2021-06-01 00:00:00], {0,0}},
+          {~N[2021-07-01 00:00:00], {1,0}},
+          {~N[2021-08-01 00:00:00], {0,1}}
+        ]
     end
 
     test "returns correct results for user time zone if snippet created just after month rollover in local time" do
-
+      # This snippet should be in bucket for their insert time, since they were inserted in "our" timezone
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2020-09-01 00:01:00.000000], @test_zone) |> DateTime.shift_zone!(@utc_zone)})
+      insert!(:snippet, %{private: false, inserted_at: Timex.to_datetime(~N[2021-07-01 00:01:00.000000], @test_zone) |> DateTime.shift_zone!(@utc_zone)})
+      insert!(:snippet, %{private: true, inserted_at: Timex.to_datetime(~N[2021-08-01 00:01:00.000000], @test_zone) |> DateTime.shift_zone!(@utc_zone)})
+      
+      Clock.freeze(~U[2021-08-15 12:00:00.000000Z])
+      assert Stats.count_per_month == [
+          {~N[2020-09-01 00:00:00], {1,0}},
+          {~N[2020-10-01 00:00:00], {0,0}},
+          {~N[2020-11-01 00:00:00], {0,0}},
+          {~N[2020-12-01 00:00:00], {0,0}},
+          {~N[2021-01-01 00:00:00], {0,0}},
+          {~N[2021-02-01 00:00:00], {0,0}},
+          {~N[2021-03-01 00:00:00], {0,0}},
+          {~N[2021-04-01 00:00:00], {0,0}},
+          {~N[2021-05-01 00:00:00], {0,0}},
+          {~N[2021-06-01 00:00:00], {0,0}},
+          {~N[2021-07-01 00:00:00], {1,0}},
+          {~N[2021-08-01 00:00:00], {0,1}}
+        ]
     end
   end
 end
