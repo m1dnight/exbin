d182142613af220c519774cb594a378a422616a4
Josh Proehl <josh@daedalusdreams.com>
2021-08-21 11:55:12 -0500

    Resolve compiler warning for page controller

--- lib/exbin_web/live/page_live.ex
+++ lib/exbin_web/live/page_live.ex
@@ -7,6 +7,7 @@ defmodule ExBinWeb.PageLive do
     {:ok, assign(socket, query: "", snippets: [])}
   end
 
+  @impl true
   def handle_event("suggest", %{"q" => query}, socket) do
     snippets = ExBin.Snippets.search(query)
     Logger.debug("#{Enum.count(snippets)} results for query `#{query}`")
