3d930c330ffc1c62edb2980b4d60c8106f4c5502
Christophe De Troyer <christophe.detroyer@gmail.com>
2021-08-25 15:25:31 +0200

    bump version ; create release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBin.MixProject do
   def project do
     [
       app: :exbin,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
